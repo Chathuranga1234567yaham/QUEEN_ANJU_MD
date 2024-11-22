bf20d36ba695eef82a79efb98dc2bfe34a479478
Chathuranga1234567yaham <boisajains@gmail.com>
2024-11-22 14:38:33 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=LElV2LZI#ySybkzXrjnj-o_ZynK5bB7U3l_mokNpAUetcIRD0kI8",
 };
